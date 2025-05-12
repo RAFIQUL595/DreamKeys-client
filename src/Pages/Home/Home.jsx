44e0af0f44da9119eaad1b9ce4fdbfdf931a2011
Rafiqul Islam <rafiqulislam42002@gmail.com>
2025-05-12 23:12:13 +0600

    advertisement section done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Advertisement from "./Advertisement";
 import Hero from "./Hero";
 
 const Home = () => {
   return (
     <div className="dark:bg-[#0B0716] bg-gray-50 ">
-        <Hero></Hero>
+      <Hero></Hero>
+      <Advertisement></Advertisement>
     </div>
   );
 };
